7bb1700e2a83ba4c12e415e1279f501620008c30
sbeland-hub <s.beland@videotron.ca>
2026-09-05 09:07:20 -0400

    Add files via upload
    Bloc mise au point, itinéraire et bandeau hors réseau

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    téléphones à récupérer la nouvelle version au lieu de garder l'ancienne.
    =========================================================================== */
 
-const VERSION = "camping-riviere-ouelle-v7";
+const VERSION = "camping-riviere-ouelle-v12";
 
 // Les fichiers gardés sur le téléphone pour fonctionner sans réseau.
 const FICHIERS = [
